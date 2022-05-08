531ec15fc91574c284dcd8001f987d8d6647aa39
Marko Dojkić <marko.dojkic@gmail.com>
2022-05-08 22:30:09 +0200

    mend

--- iot-webserver.py
+++ iot-webserver.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = 'castleville.crowns@gmail.com'
-app.config['MAIL_PASSWORD'] = "Alastair"
+app.config['MAIL_PASSWORD'] = "ebnhgwbgvurtelun"
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 
