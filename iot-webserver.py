1130b564c5b7afc4a79e95c1577b83434778d4c3
Marko Dojkić <marko.dojkic@gmail.com>
2022-05-08 22:10:10 +0200

    Email tls/ssl fix

--- iot-webserver.py
+++ iot-webserver.py
@@ -14,8 +14,8 @@ app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = 'castleville.crowns@gmail.com'
 app.config['MAIL_PASSWORD'] = "Alastair"
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
 
 CORS(app)
 cors = CORS(app, resource={
