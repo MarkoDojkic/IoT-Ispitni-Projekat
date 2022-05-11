3b70023fec10c706b726e0f082af41a726ec3168
Marko Dojkić <marko.dojkic@gmail.com>
2022-05-11 12:20:20 +0200

    mned

--- iot-webserver.py
+++ iot-webserver.py
@@ -56,13 +56,13 @@ def dashboard():
 @app.route('/getCommand', methods=['GET'])
 def getCommand():
     global command
-    """
+
     app.response_class(
         response=json.dumps(command),
         status=200,
         mimetype='application/json'
     )
-    """
+
     return command
 
 @app.route('/clearCommand', methods=['GET'])
